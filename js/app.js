3dd07b1d7cc746d6bbfeee46921b50fad2661471
Mary Francis <maryfsc@disroot.org>
2018-08-04 17:09:33 -0300

    Modal com infos dos restaurantes.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,35 @@
 $(document).ready(function () {
 
   $('#logo-page').delay('2500').fadeOut('slow');
-
-  $('#initial-page').hide();
-  $('#initial-page').delay('2500').fadeIn('slow');
-
-  var map;
-
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map-wrapper'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-  }
+  $('#initial-page').hide().delay('2500').fadeIn('slow');
 
   initMap();
 
-  restaurantes.forEach(restaurante => {
-    $("#pics-wrapper").append('<img src=' + restaurante.image + ' class="">');
+  restaurantes.forEach((restaurant, index) => {
+    $('#pics-wrapper').append('<img data-toggle="modal" data-target="#only-modal" data-whatever=' + restaurant.name + ' src=' + restaurant.image + ' id="rest' + index + '" class=' + restaurant.type + '>');
+    
+    $('#rest' + index).on('click', function () {
+      $('#modal-restname').html('<h3 class="text-center">' + restaurant.name + '</h3>');
+      $('#modal-body').html('<img class="align-self-center" src=' + restaurant.image + ' height="150"><p class="mx-4">' + restaurant.description + '<p>');
+    })
   });
 
-  // debugger
-  // $.getJSON('data.js', function(restaurantes) {
-  //   $.each(restaurantes, function(src) {
-  //     $('#pics-wrapper').append('<img src=' + restaurantes[image] + '>');
-  //   });
-  // });
 
-//   $.getJSON('data.js', function(image) {
-//   var restaurantPics = [];
-//   $.each( image, function(val) {
-//     restaurantPics.push('<img src="' + val + '">');
-//   });
- 
-//   $( "<ul/>", {
-//     "class": "my-new-list",
-//     html: restaurantPics.join( "" )
-//   }).appendTo('#pics-wrapper');
-// });
+  // $('#user-input').on('input', function () {
+  //   restaurantes.forEach(restaurant => {
+  //     if ($(this).val() === restaurant.name || $(this).val(). === restaurant.type) {
+  //       console.log('aeee');
+  //     }
+  //   })
+  // })
 
 });
 
+var map;
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map-wrapper'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
