9dc1688ed3b0168dbe5bf99961ecc6984c51ed93
Mary Francis <maryfsc@disroot.org>
2018-08-04 20:58:48 -0300

    Filtro por nome de restaurante e tipo de comida.

--- js/app.js
+++ js/app.js
@@ -5,25 +5,42 @@ $(document).ready(function () {
 
   initMap();
 
+  $('#error-message').hide();
+
   restaurantes.forEach((restaurant, index) => {
     $('#pics-wrapper').append('<img data-toggle="modal" data-target="#only-modal" data-whatever=' + restaurant.name + ' src=' + restaurant.image + ' id="rest' + index + '" class=' + restaurant.type + '>');
     
     $('#rest' + index).on('click', function () {
       $('#modal-restname').html('<h3 class="text-center">' + restaurant.name + '</h3>');
       $('#modal-body').html('<img class="align-self-center" src=' + restaurant.image + ' height="150"><p class="mx-4">' + restaurant.description + '<p>');
-    })
-  });
+    });
+  }); // termina arrow function primeiro forEach
 
+  $('#filter-button').on('click', function () {
+    $('#error-message').hide();
+    $('#message').show();
+    var userInput = $('#user-input').val();
+    var count = 0;
 
-  // $('#user-input').on('input', function () {
-  //   restaurantes.forEach(restaurant => {
-  //     if ($(this).val() === restaurant.name || $(this).val(). === restaurant.type) {
-  //       console.log('aeee');
-  //     }
-  //   })
-  // })
+    restaurantes.forEach((restaurant, index) => {
+      if (userInput !== restaurant.name && userInput !== restaurant.type) {
+        $('#rest' + index).hide();
+      } else {
+        $('#rest' + index).show();
+        count += 1;
+      } // termina if
+    }) // termina arrow function segundo for each
 
-});
+    if (count === 0) {
+      $('#error-message').show();
+      $('#message').hide();
+
+      restaurantes.forEach((restaurant, index) => {
+        $('#rest' + index).show();  
+      });
+    };
+  }); // termina evento com button
+}); // termina jquery
 
 var map;
 
